d71ea47b94e67f054e59a1e19f23260a85569eaa
Iiro Valkoja <iiro.valkoja@courses.tamk.fi>
2018-10-02 13:41:00 +0300

    Map markers and panning

--- Mockup/script/map.js
+++ Mockup/script/map.js
@@ -1,5 +1,10 @@
 'use strict';
 
+function mapCallback()
+{
+    window.map = new Map();
+}
+
 class Map
 {
     constructor()
@@ -10,27 +15,23 @@ class Map
         this.map = new google.maps.Map(this.$map.get(0), {'center': new google.maps.LatLng(61.502545, 23.807463), zoom: 16});
 
         this.$map.height($(document).height() + 'px');
+
+        let marker1 = new google.maps.Marker({'position': new google.maps.LatLng(61.502451, 23.806198), map: this.map});
+            marker1.addListener('click', (aEvent) => {this.handleClick(aEvent, 0)}, false);
+
+        let marker2 = new google.maps.Marker({'position': new google.maps.LatLng(61.502249, 23.807944), map: this.map});
+            marker2.addListener('click', (aEvent) => {this.handleClick(aEvent, 1)}, false);
+
+        let marker3 = new google.maps.Marker({'position': new google.maps.LatLng(61.502742, 23.805301), map: this.map});
+            marker3.addListener('click', (aEvent) => {this.handleClick(aEvent, 2)}, false);
+
+        let marker4 = new google.maps.Marker({'position': new google.maps.LatLng(61.503110, 23.812554), map: this.map});
+            marker4.addListener('click', (aEvent) => {this.handleClick(aEvent, 3)}, false);
     }
 
-
+    handleClick(aEvent, aID)
+    {
+        console.log(aID);
+        this.map.panTo(aEvent.latLng);
+    }
 }
-
-
-
-/*
-RotationMap.prototype.initMap = function(aElement)
-{
-    this.map = new google.maps.Map(document.getElementById(aElement), {center: new google.maps.LatLng(60, 20), zoom: 5, minZoom: 3, maxZoom: 8});
-    this.scale = 1 / (Math.pow(2, -(this.map.getZoom())));
-
-    this.createPortMarkers();
-    this.createLineMarkers();
-
-    google.maps.event.addListener(this.map, 'projection_changed', function() {this.updateLines()}.bind(this));
-    google.maps.event.addListener(this.map, 'zoom_changed', function() {this.updateScales()}.bind(this));
-};
-
-
-let marker = {id: id, marker: new google.maps.Marker({position: new google.maps.LatLng(aLat, aLng), label: aLabel, zIndex: this.idCount, draggable: true, map: this.map})};
-    marker.marker.addListener('dragend', (aEvent) => {this.handleDrag(aEvent, marker.id)}, false);
-*/
