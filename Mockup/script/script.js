15be863384fc7d43cf5a253700d18c086303e8a5
Teemu Uusi-Rasi <teemu.uusi-rasi@eng.tamk.fi>
2018-10-05 08:31:30 +0300

    separate img for each state and button

--- Mockup/script/script.js
+++ Mockup/script/script.js
@@ -2,7 +2,7 @@ var currentSite = 'favs';
 
 function changeSite(newSite) {
 	document.getElementById('content-view').src='./src/' + newSite + '.html';
-	document.getElementsByClassName('button-' + currentSite)[0].src='./img/test_button_n_pressed.png';
-	document.getElementsByClassName('button-' + newSite)[0].src='./img/test_button_pressed.png';
+	document.getElementsByClassName('button-' + currentSite)[0].src='./img/test_button_' + currentSite + '_n.png';
+	document.getElementsByClassName('button-' + newSite)[0].src='./img/test_button_' + newSite + '.png';
 	currentSite = newSite;
 }
