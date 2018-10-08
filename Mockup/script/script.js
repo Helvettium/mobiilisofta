3250ed370ef65a741f85627c1729169929d32c7e
Valkoja Iiro <iiro.valkoja@courses.tamk.fi>
2018-10-08 08:41:29 +0300

    Container size and script error fixes

--- Mockup/script/script.js
+++ Mockup/script/script.js
@@ -1,17 +1,25 @@
 var currentSite = 'favs';
 
 function changeSite(newSite) {
-    switch (newSite) {
-        case 'map':
-            document.getElementById('mapIcon').style.display = 'none';
-            break;
-        default:
-            document.getElementById('mapIcon').style.display = 'block';
-            break;
+	document.getElementById('content-view').src='./src/' + newSite + '.html';
+    
+    let newButton = document.getElementsByClassName('button-' + newSite);
+    let oldButton = document.getElementsByClassName('button-' + currentSite);
+
+    if (typeof newButton[0] !== 'undefined') {
+        document.getElementsByClassName('button-' + newSite)[0].src='./img/test_button_' + newSite + '.png';
+    }
+    
+    if (typeof oldButton[0] !== 'undefined') {
+        document.getElementsByClassName('button-' + currentSite)[0].src='./img/test_button_' + currentSite + '_n.png';
+    }
+    
+    if (newSite == 'map') {
+        document.getElementById('mapIcon').style.display = 'none';
+    }
+    else {
+        document.getElementById('mapIcon').style.display = 'block';
     }
 
-	document.getElementById('content-view').src='./src/' + newSite + '.html';
-	document.getElementsByClassName('button-' + currentSite)[0].src='./img/test_button_' + currentSite + '_n.png';
-	document.getElementsByClassName('button-' + newSite)[0].src='./img/test_button_' + newSite + '.png';
 	currentSite = newSite;
 }
