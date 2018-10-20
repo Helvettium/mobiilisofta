466ade7f65f4b211a9d2f1ddb3d7f09cd1ad7a14
Teemu Uusi-Rasi <teemu.uusi-rasi@eng.tamk.fi>
2018-10-20 14:02:07 +0300

    jotain tein mutta unohdin mitä

---
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/Favourites.kt
@@ -0,0 +1,24 @@
+package asia.jokin.ohjelmistomobiili
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_favourites.*
+
+class Favourites : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favourites)
+        setSupportActionBar(toolbar)
+
+
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/MainActivity.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/MainActivity.kt
@@ -1,12 +1,22 @@
 package asia.jokin.ohjelmistomobiili
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getWindow().decorView.setBackgroundColor(resources.getColor(R.color.colorPrimaryDark)) // IDE kaski kayttaa getColor ja sit se on deprecated >:U
+
+        //Tassa valissa haetaan tiedot ja odotellaan kaynnistymista
+
+        Thread.sleep(2000) // TODO poista kun lisataan jotakin tekemista talle aktiviteetille
+
+        val startupIntent = Intent(this, Favourites::class.java)
+        startActivity(startupIntent)
     }
 }
