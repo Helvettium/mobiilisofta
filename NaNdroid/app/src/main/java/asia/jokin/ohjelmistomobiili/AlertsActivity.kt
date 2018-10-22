414caf51ed25fc3e23d328ff108203b126d24163
Teemu Uusi-Rasi <teemu.uusi-rasi@eng.tamk.fi>
2018-10-22 19:28:28 +0300

    alerts tehty

--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/AlertsActivity.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/AlertsActivity.kt
@@ -2,11 +2,36 @@ package asia.jokin.ohjelmistomobiili
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class AlertsActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alerts)
+
+        val exampleDataTable: Array<String> = arrayOf("Alert1","Alert2","Alert3","Alert4") // TODO REMOVE
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = AlertCardAdapter(exampleDataTable)
+
+        recyclerView = findViewById<RecyclerView>(R.id.alertRecycle).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
+        }
+
     }
 }
