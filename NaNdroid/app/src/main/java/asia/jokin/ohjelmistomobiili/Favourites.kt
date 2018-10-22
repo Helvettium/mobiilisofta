2c7ef98383f222c8487136c276dce692aedcec6f
Teemu Uusi-Rasi <teemu.uusi-rasi@eng.tamk.fi>
2018-10-22 10:49:40 +0300

    some changes

--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/Favourites.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/Favourites.kt
@@ -2,18 +2,22 @@ package asia.jokin.ohjelmistomobiili
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 import kotlinx.android.synthetic.main.activity_favourites.*
 
 class Favourites : AppCompatActivity() {
 
+    val favButton = findViewById<Button>(R.id.favsButton)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favourites)
-        setSupportActionBar(toolbar)
 
-
+        favButton.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/MainActivity.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/MainActivity.kt
@@ -10,11 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getWindow().decorView.setBackgroundColor(resources.getColor(R.color.colorPrimaryDark)) // IDE kaski kayttaa getColor ja sit se on deprecated >:U
+        this.window.decorView.setBackgroundColor(resources.getColor(R.color.colorPrimaryDark)) // IDE kaski kayttaa getColor ja sit se on deprecated >:U
 
-        //Tassa valissa haetaan tiedot ja odotellaan kaynnistymista
 
-        Thread.sleep(2000) // TODO poista kun lisataan jotakin tekemista talle aktiviteetille
 
         val startupIntent = Intent(this, Favourites::class.java)
         startActivity(startupIntent)
