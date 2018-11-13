2748fe33ca17211b004296c44200fb8c42cc05b5
Teemu Uusi-Rasi <teemu.uusi-rasi@eng.tamk.fi>
2018-11-13 12:25:59 +0200

    commit

--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/FetchDataSingleton.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/FetchDataSingleton.kt
@@ -1,12 +1,25 @@
 package asia.jokin.ohjelmistomobiili
 
+import android.app.Activity
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.util.ArrayList
+
 data class StopsData(val data1: String)
 data class LineData(val data1: String)
 data class StopData(val data1: String)
 
-object FetchDataSingleton {
+object FetchDataSingleton : Activity(){
+
     private val BASE_URL: String = "http://api.publictransport.tampere.fi/prod/?user=zx123&pass=qmF:L}h3wR2n"
     private var endUrl: String = ""
+    val stopData: ArrayList<StopData> = ArrayList()
+    private val stopsData = ArrayList<StopsData>()
+    private val lineData = ArrayList<LineData>()
+    //private lateinit var appContext: Context
     // TODO IMPLEMENTOI
     // LocationSingleton.checkPermissionForLocation
 
@@ -18,15 +31,23 @@ object FetchDataSingleton {
 
     }
 
-    fun getStopData(locationLatitude: String? = null, locationLongitude: String? = null){
-
+    fun getStopData(locationName: String){
+        val searchTerm = "&code=$locationName"
+        fetchData(searchTerm)
     }
 
-    fun updateUrl(searchTerm: String = "&key=met"){
+    private fun fetchData(searchTerm: String){
+        val queue = Volley.newRequestQueue(this)
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(Request.Method.GET, BASE_URL+searchTerm,
+                Response.Listener { response -> getJson(response) }, Response.ErrorListener { error -> Toast.makeText(this, error.toString(), Toast.LENGTH_SHORT).show() })
 
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
-
-    fun fetchData(locationLatitude: String? = null, locationLongitude: String? = null){
-
+    private fun getJson(response: String) {
+        // TODO move elsewhere
+        stopData.clear()
+        stopData.add(StopData(response))
     }
 }
