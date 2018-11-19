8e94fb35a340a1d59ce3a54bc1aab1ee6023c8da
Teemu Uusi-Rasi <teemu.uusi-rasi@eng.tamk.fi>
2018-11-19 12:28:22 +0200

    asd

--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/StopsFragment.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/StopsFragment.kt
@@ -1,5 +1,7 @@
 package asia.jokin.ohjelmistomobiili
 
+import android.content.Context
+import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -7,12 +9,17 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.gms.maps.model.LatLng
+import org.json.JSONArray
 
 class StopsFragment : Fragment() {
 
+    private val testLocation: LatLng = LatLng(68.2554300, 33.2764400)
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private var fetchManager = FetchDataSingleton
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -21,6 +28,14 @@ class StopsFragment : Fragment() {
         //textView.setText("In stops")
 
         val exampleDataTable: Array<String> = arrayOf("Stop 1","Stop 2","Stop 3","Stop 4") // TODO remove
+        val fetchedData: Array<String>
+
+        fetchManager.getInstance(activity!!.application).getStopsData(testLocation, object: DataCallback{
+            override fun onSuccess(response: JSONArray, context: Context) {
+                Toast.makeText(context, response.toString(), Toast.LENGTH_LONG).show()
+            }
+        })
+
 
         viewManager = LinearLayoutManager(activity)
         viewAdapter = StopsAdapter(exampleDataTable)
