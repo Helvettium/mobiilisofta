d211781b036062047c23935bd15c094f0134df0d
Iiro Valkoja <iiro.valkoja@courses.tamk.fi>
2018-11-29 15:21:09 +0200

    Cleanup

--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/MapsActivity.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/MapsActivity.kt
@@ -28,14 +28,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         // TODO REMOVE v
-        val stopid: Int = intent.getIntExtra("stopid",0)
-        if (0!=stopid){
+        val stopid: Int = intent.getIntExtra("stopid", 0)
+
+        if (stopid != 0) {
             val popupIntent = Intent(this, PopupActivity::class.java)
             popupIntent.putExtra("stopid", stopid.toString())
             startActivity(popupIntent)
@@ -65,13 +66,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
 
 
-        val testLocation = LatLng(61.4980000, 23.7604000)
+        val testLocation = LatLng(LocationSingleton.getLat(), LocationSingleton.getLng())
         val testMarker1 = LatLng(61.4980214, 23.7603118)
         val testMarker2 = LatLng(61.5040000, 23.7593000)
         val testMarker3 = LatLng(61.4960214, 23.7599118)
         val clickableCircle1 = CircleOptions().center(testMarker1).clickable(true).visible(true).radius(20.0)
         val clickableCircle2 = CircleOptions().center(testMarker2).clickable(true).visible(true).radius(20.0)
         val clickableCircle3 = CircleOptions().center(testMarker3).clickable(true).visible(true).radius(20.0)
+
         mMap.addCircle(clickableCircle1).run{tag = "0035"}
         mMap.addCircle(clickableCircle2).run{tag = "0035"}
         mMap.addCircle(clickableCircle3).run{tag = "0035"}
@@ -86,8 +88,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         /*
         TODO tassa vain esimerkkikoodia, bussit tulevat valmiissa softassa toisaalle
         johonkin funktioon, joka latautuu aina nakyman muuttuessa
-
         */
+        
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(testLocation, 16.0F))
 
         // mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(defLat, defLng)))
