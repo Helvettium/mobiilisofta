a8912c05ca69dab5b32a51a26c81badfd4e170b4
Iiro Valkoja <iiro.valkoja@courses.tamk.fi>
2018-11-30 01:27:26 +0200

    Initial location, initial stop

--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/MapsActivity.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/MapsActivity.kt
@@ -1,23 +1,18 @@
 package asia.jokin.ohjelmistomobiili
 
 import android.content.Intent
-import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.GoogleMap.MAP_TYPE_NORMAL
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import org.json.JSONObject
-import android.R.attr.password
 import android.content.Context
 import android.util.Log
 import com.google.android.gms.maps.model.*
 import org.json.JSONArray
-
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCameraIdleListener, GoogleMap.OnMarkerClickListener {
     private lateinit var mMap: GoogleMap
@@ -29,30 +24,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCamera
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
-        // TODO REMOVE v
-        val stopid: Int = intent.getIntExtra("stopid", 0)
-
-        if (stopid != 0) {
-            val popupIntent = Intent(this, PopupActivity::class.java)
-            popupIntent.putExtra("stopid", stopid.toString())
-            startActivity(popupIntent)
-        }
-        // TODO REMOVE ^
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
-        // Mihin kartan pitäisi osoittaa
-        val curLatLng = LatLng(LocationSingleton.getLat(), LocationSingleton.getLng())
+        // Mihin kartan pitäisi alussa osoittaa
+        val lat = intent.getDoubleExtra("lat", LocationSingleton.getLat())
+        val lng = intent.getDoubleExtra("lng", LocationSingleton.getLng())
+
+        val curLatLng = LatLng(lat, lng)
 
         // Kartta itse
         mMap = googleMap
@@ -60,56 +39,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCamera
         mMap.setOnCameraIdleListener(this)
         mMap.setOnMarkerClickListener(this)
 
+        // Avataan mahdollinen valittu pysäkki
+        val stopCode = intent.getIntExtra("stopid", 0)
+
+        if (stopCode != 0) {
+            openStop(stopCode.toString())
+        }
+
         // Päivitetään pysäkit
         updateStops(curLatLng)
-        
-        
-        // Default location values (keskustori)
 
-        // val defLoc: Location = LocationSingleton.mLastLocation
-        // val defLat: Double = intent.getDoubleExtra("lat", defLoc.getLatitude())
-        // val defLng: Double = intent.getDoubleExtra("lng", defLoc.getLongitude())
-
-
-
-
-
-/*
-        //val testLocation = LatLng(LocationSingleton.getLat(), LocationSingleton.getLng())
-        val testMarker1 = LatLng(61.4980214, 23.7603118)
-        val testMarker2 = LatLng(61.5040000, 23.7593000)
-        val testMarker3 = LatLng(61.4960214, 23.7599118)
-        val clickableCircle1 = CircleOptions().center(testMarker1).clickable(true).visible(true).radius(20.0)
-        val clickableCircle2 = CircleOptions().center(testMarker2).clickable(true).visible(true).radius(20.0)
-        val clickableCircle3 = CircleOptions().center(testMarker3).clickable(true).visible(true).radius(20.0)
-
-        mMap.addCircle(clickableCircle1).run{tag = "0035"}
-        mMap.addCircle(clickableCircle2).run{tag = "0035"}
-        mMap.addCircle(clickableCircle3).run{tag = "0035"}
-*/
-
-
-        /*
-        TODO tassa vain esimerkkikoodia, bussit tulevat valmiissa softassa toisaalle
-        johonkin funktioon, joka latautuu aina nakyman muuttuessa
-        */
-        
-        
-
-        // mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(defLat, defLng)))
-
-/*
-        with(mMap) {
-            setOnCircleClickListener {
-                val popupIntent = Intent(this@MapsActivity, PopupActivity::class.java)
-                popupIntent.putExtra("stopid", it.tag.toString().toInt()) // TODO mielimmun anna pysäkin code kun sitä käytetään data haussa
-                //Toast.makeText(this@MapsActivity, it.tag.toString(),Toast.LENGTH_LONG).show()
-                startActivity(popupIntent)
-            }
-        }
-*/
+        // Toast.makeText(context, mMarker.tag.toString(), Toast.LENGTH_LONG).show()
     }
-
 
     override fun onCameraIdle() {
         // Minne kartta osoittaa
@@ -119,6 +60,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCamera
 
         // Päivitetään pysäkit
         updateStops(curLatLng)
+    }
+
+    override fun onMarkerClick(mMarker: Marker): Boolean {
+        // Debug
+        Log.d("Marker", mMarker.tag.toString())
+
+        openStop((mMarker.tag as JSONObject).getString("code"))
+
+        return true
     }
 
     private fun updateStops(mLatLng: LatLng) {
@@ -145,13 +95,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCamera
         })
     }
 
-
-    override fun onMarkerClick(mMarker: Marker?): Boolean {
-
-        //Toast.makeText(context, mMarker.tag.toString(), Toast.LENGTH_LONG).show()
-        Log.d("Marker", mMarker?.tag.toString())
-
-        return true
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun openStop(mCode: String) {
+        val popupIntent = Intent(this@MapsActivity, PopupActivity::class.java)
+        popupIntent.putExtra("stopid", mCode)
+        startActivity(popupIntent)
     }
 }
