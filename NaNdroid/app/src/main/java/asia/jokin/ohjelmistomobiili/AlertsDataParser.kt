c3f6e1121da5edcaceb13b9ddd84e0257aebb71e
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-12-04 12:49:10 +0200

    Catch errors in parsing

--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/AlertsDataParser.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/AlertsDataParser.kt
@@ -2,6 +2,7 @@ package asia.jokin.ohjelmistomobiili
 
 import org.json.JSONObject
 import android.content.Context
+import org.json.JSONException
 
 interface AlertDataCallback {
     fun onSuccess(response: ArrayList<Alert>, context: Context)
@@ -9,13 +10,19 @@ interface AlertDataCallback {
 
 fun parseData(data: JSONObject): ArrayList<Alert> {
     val alerts: ArrayList<Alert> = ArrayList()
-    val messages = data.getJSONObject("Siri").getJSONObject("ServiceDelivery").getJSONArray("GeneralMessageDelivery")
-    for(i in 0..(messages.length() - 1)) {
-        val message = messages.getJSONObject(0).getJSONArray("GeneralMessage").getJSONObject(0)
-        val recordedAt = message.getLong("RecordedAtTime")
-        val validUntil = message.getLong("ValidUntilTime")
-        val content = message.getString("Content")
-        alerts.add(Alert(recordedAt, validUntil, content))
+    try {
+        val messages = data.getJSONObject("Siri").getJSONObject("ServiceDelivery").getJSONArray("GeneralMessageDelivery")
+        for(i in 0..(messages.length() - 1)) {
+            val message = messages.getJSONObject(0).getJSONArray("GeneralMessage").getJSONObject(0)
+            val recordedAt = message.getLong("RecordedAtTime")
+            val validUntil = message.getLong("ValidUntilTime")
+            val content = message.getString("Content")
+            alerts.add(Alert(recordedAt, validUntil, content))
+        }
     }
+    catch (e: JSONException) {
+        println("Empty response, return empty array")
+    }
+
     return alerts
 }
