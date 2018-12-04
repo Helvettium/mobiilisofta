be6375d0985e6420ec95df5b17d908ea88e23247
Teemu Uusi-Rasi <teemu.uusi-rasi@eng.tamk.fi>
2018-12-04 15:15:19 +0200

    hidden unused settings and fixed preferencesummary

--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/SettingsActivity.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/SettingsActivity.kt
@@ -82,8 +82,8 @@ class SettingsActivity : AppCompatPreferenceActivity() {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("example_text"))
-            bindPreferenceSummaryToValue(findPreference("example_list"))
+            bindPreferenceSummaryToValue(findPreference("times_shown"))
+            bindPreferenceSummaryToValue(findPreference("nearby"))
         }
 
         override fun onOptionsItemSelected(item: MenuItem): Boolean {
