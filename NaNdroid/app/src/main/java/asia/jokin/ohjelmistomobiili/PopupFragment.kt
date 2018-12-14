fc7b832d0127e6c0502f4b9246ad4d0fad2ecbec
Iiro Valkoja <iiro.valkoja@courses.tamk.fi>
2018-12-14 16:24:44 +0200

    Popup fragment init

---
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/PopupFragment.kt
@@ -0,0 +1,13 @@
+package asia.jokin.ohjelmistomobiili
+
+import android.app.Fragment
+import android.os.Bundle
+
+class PopupFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
