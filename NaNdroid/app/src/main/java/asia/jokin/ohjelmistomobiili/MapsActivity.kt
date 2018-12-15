6292bab5cd35bafa1904d88ab794f137fcc9f0bb
Iiro Valkoja <iiro.valkoja@courses.tamk.fi>
2018-12-15 20:55:38 +0200

    Yet more cleanup..

--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/MapsActivity.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/MapsActivity.kt
@@ -1,5 +1,6 @@
 package asia.jokin.ohjelmistomobiili
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,11 +8,8 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import org.json.JSONObject
-import android.content.Context
-import android.util.Log
-import android.widget.Toast
 import com.google.android.gms.maps.model.*
+import org.json.JSONObject
 import org.json.JSONArray
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCameraIdleListener, GoogleMap.OnMarkerClickListener {
@@ -23,11 +21,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCamera
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
-        // Alustetaan muuttujat, piilotetaan pysäkki-popup
+        // Alustetaan & piilotetaan pysäkki-popup
         mPopup = supportFragmentManager.findFragmentById(R.id.popup) as PopupFragment
         supportFragmentManager.beginTransaction().hide(mPopup).commit()
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Kartta-fragment, ilmoitus itselle kun se on valmis
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
@@ -36,12 +34,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCamera
         // Mihin kartan pitäisi alussa osoittaa
         val lat = intent.getDoubleExtra("lat", LocationSingleton.getLat())
         val lng = intent.getDoubleExtra("lng", LocationSingleton.getLng())
-
-        val curLatLng = LatLng(lat, lng)
+        val newLatLng = LatLng(lat, lng)
 
         // Kartta itse
         mMap = googleMap
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(LocationSingleton.getLat(), LocationSingleton.getLng()), 16.0F))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newLatLng, 16.0F))
         mMap.setOnCameraIdleListener(this)
         mMap.setOnMarkerClickListener(this)
 
@@ -49,11 +46,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCamera
         val stopCode = intent.getIntExtra("stopid", 0)
 
         if (stopCode != 0) {
-            openStop(stopCode.toString())
+            openStop(stopCode)
         }
 
         // Päivitetään pysäkit
-        updateStops(curLatLng)
+        updateStops(newLatLng)
 
         // Toast.makeText(context, mMarker.tag.toString(), Toast.LENGTH_LONG).show()
     }
@@ -69,17 +66,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCamera
     }
 
     override fun onMarkerClick(mMarker: Marker): Boolean {
-        // Debug
-        Log.d("Marker", mMarker.tag.toString())
-
-        openStop((mMarker.tag as JSONObject).getString("code"))
+        // Avataan valittu pysäkki, geneerinen tag -> json -> "code"-string -> int
+        openStop((mMarker.tag as JSONObject).getString("code").toInt())
 
         return true
     }
 
     private fun updateStops(aLatLng: LatLng) {
         // Haetaan pysäkit
-        FetchDataSingleton.getInstance(this.applicationContext).getStopsData(aLatLng, 1000, object: DataCallback{
+        FetchDataSingleton.getInstance(this.applicationContext).getStopsData(aLatLng, 1000, object: DataCallback {
             override fun onSuccess(response: JSONArray, context: Context) {
 
                 // Iteroidaan palautettu lista
@@ -107,18 +102,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCamera
         })
     }
 
-    private fun openStop(aCode: String) {
-
-        //Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
-
-        // Vitun raivostuttava tapa viitata ID
-        // popupTitle.text = aCode
-
-        //val popupFragment = supportFragmentManager.findFragmentById(R.id.popup) as PopupFragment
-        //supportFragmentManager.beginTransaction().hide(popupFragment).commit()
-
+    private fun openStop(aCode: Int) {
+        // Näytetään pysäkki
         mPopup.showStop(aCode)
 
+        // Jos fragment on vielä piilossa, tuodaan se näkyviin
         if (mPopup.isHidden) {
             supportFragmentManager.beginTransaction()
                     .setCustomAnimations(R.animator.popup_show, android.R.animator.fade_out)
--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/PopupFragment.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/PopupFragment.kt
@@ -3,6 +3,7 @@ package asia.jokin.ohjelmistomobiili
 import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,20 +16,23 @@ class PopupFragment : Fragment() {
     private lateinit var mViewManager: RecyclerView.LayoutManager
 
     override fun onCreateView(aInflater: LayoutInflater, aContainer: ViewGroup?, aSavedInstanceState: Bundle?): View? {
-
         return aInflater.inflate(R.layout.popup_fragment, aContainer, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onViewCreated(aView: View, aSavedInstanceState: Bundle?) {
+        super.onViewCreated(aView, aSavedInstanceState)
 
-        mTitleText = view.findViewById(R.id.popupTitle)
+        mTitleText = aView.findViewById(R.id.popupTitle)
     }
 
-    fun showStop(aStopCode: String) {
+    fun showStop(aStopCode: Int) {
+
+        // Debug
+        Log.d("Marker", aStopCode.toString())
+
         //Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
 
         // Vitun raivostuttava tapa viitata ID
-        mTitleText.text = aStopCode
+        // mTitleText.text = aStopCode
     }
 }
