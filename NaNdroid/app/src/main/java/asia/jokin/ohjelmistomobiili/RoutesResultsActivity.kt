a662cde90e8fdd926c050cb75525b59914281481
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-12-18 02:35:26 +0200

    Add map functionality

--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/RoutesResultsActivity.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/RoutesResultsActivity.kt
@@ -1,16 +1,27 @@
 package asia.jokin.ohjelmistomobiili
 
-import android.content.res.TypedArray
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Parcelable
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.Button
+import android.widget.ImageButton
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 
-class RouteResultsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnCameraIdleListener, GoogleMap.OnMarkerClickListener {
+
+
+
+
+class RouteResultsActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var mContext = this
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
@@ -18,27 +29,45 @@ class RouteResultsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.
     private lateinit var mapView: SupportMapFragment
     private lateinit var mMap: GoogleMap
 
+    private lateinit var mData: List<Route>
+    private var mapFullScreen: Boolean = false
+    private var originalHeight: Int? = 0
+    private var originalWidth: Int? = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.routes_results_activity)
         viewManager = LinearLayoutManager(this)
+        mData = intent.getParcelableArrayListExtra("resultsData")
 
-        mapView = supportFragmentManager.findFragmentById(R.id.routeInfoMap) as SupportMapFragment
-        mapView.getMapAsync(this)
-
-        val data = intent.getParcelableArrayListExtra<Route>("resultsData")
-
-        viewAdapter = RoutesResultsCardAdapter(data, this)
+        viewAdapter = RoutesResultsCardAdapter(mData, this) { route: Route -> locationItemClicked(route) }
         recyclerView = findViewById<RecyclerView>(R.id.routeResultsRecyclerVIew).apply {
-            // use this setting to improve performance if you know that changes
-            // in content do not change the layout size of the RecyclerView
             setHasFixedSize(false)
-
-            // use a linear layout manager
             layoutManager = viewManager
-
-            // specify an viewAdapter (see also next example)
             adapter = viewAdapter
+        }
+        this.
+        mapView = supportFragmentManager.findFragmentById(R.id.routeInfoMap) as SupportMapFragment
+        mapView.getMapAsync(mContext)
+
+        val fullScreenMapButton = findViewById<ImageButton>(R.id.fullScreenMapButton)
+        fullScreenMapButton.setOnClickListener { v ->
+            mapView.view?.requestLayout()
+            mapView.view?.layoutParams?.height = this.window.decorView.height
+            mapView.view?.layoutParams?.width = this.window.decorView.width
+            mapFullScreen = !mapFullScreen
+        }
+    }
+
+    override fun onBackPressed() {
+        if(mapFullScreen) {
+            mapView.view?.requestLayout()
+            mapView.view?.layoutParams?.height = originalHeight
+            mapView.view?.layoutParams?.width = originalWidth
+            mapFullScreen = !mapFullScreen
+            // setContentView(R.layout.routes_results_activity)
+        } else {
+            finish()
         }
     }
 
@@ -50,20 +79,38 @@ class RouteResultsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.
 
         // Kartta itse
         mMap = googleMap
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(LocationSingleton.getLat(), LocationSingleton.getLng()), 16.0F))
-        mMap.setOnCameraIdleListener(this)
-        mMap.setOnMarkerClickListener(this)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLatLng, 16.0F))
+        mMap.setOnMapLoadedCallback {
+            moveMapToRoute(mData.first())
+            originalHeight = mapView.view?.height
+            originalWidth = mapView.view?.width
+        }
     }
 
-    override fun onCameraIdle() {
-        // Minne kartta osoittaa
-        val curLatLng = mMap.getCameraPosition().target
-
-        // Tutkintaa onko kartta siirtynyt tarpeeksi?
+    private fun moveMapToRoute(route: Route) {
+        mMap.clear()
+        val markers: ArrayList<LatLng> = ArrayList()
+        for(leg in route.legs) {
+            for(loc in leg.locs) {
+                markers.add(LatLng(loc.coord.y, loc.coord.x))
+            }
+        }
+        mMap.addPolyline(PolylineOptions()
+                .addAll(markers)
+                .width(10f)
+                .color(ContextCompat.getColor(applicationContext, R.color.colorPrimary)))
+        val builder = LatLngBounds.Builder()
+        builder.include(markers.first())
+        builder.include(markers[markers.size / 4])
+        builder.include(markers[markers.size / 4 * 3])
+        builder.include(markers.last())
+        val bounds = builder.build()
+        val padding = 60
+        val cu = CameraUpdateFactory.newLatLngBounds(bounds, padding)
+        mMap.animateCamera(cu)
     }
 
-    override fun onMarkerClick(mMarker: Marker): Boolean {
-
-        return true
+    private fun locationItemClicked(route: Route) {
+        moveMapToRoute(route)
     }
 }
--- NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/RoutesResultsCardAdapter.kt
+++ NaNdroid/app/src/main/java/asia/jokin/ohjelmistomobiili/RoutesResultsCardAdapter.kt
@@ -16,125 +16,117 @@ import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
-class RoutesResultsCardAdapter (private val inputData: List<Route>, context: Context):
-        RecyclerView.Adapter<RoutesResultsCardAdapter.MyViewHolder>() {
+class RoutesResultsCardAdapter (private val inputData: List<Route>, context: Context, private val clickListener: (Route) -> Unit):
+        RecyclerView.Adapter<RoutesResultsCardAdapter.RouteResultViewHolder>() {
     private val mContext = context
-    private var mInfoVisible = false
-    private var mResultSelected = false
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder.
-    // Each data item is just a string in this case that is shown in a TextView.
-    class MyViewHolder(val cardView: CardView) : RecyclerView.ViewHolder(cardView)
+    class RouteResultViewHolder(val cardView: CardView) : RecyclerView.ViewHolder(cardView) {
+        private fun combineLegs(legs: List<Leg>): ArrayList<Pair<String?, Location>> {
+            var iterator = 0
+            val locs = ArrayList<Pair<String?, Location>>()
+            while(iterator < legs.lastIndex + 1) {
+                val type = legs[iterator].code
+                var jiterator = 0
+                while(jiterator < legs[iterator].locs.lastIndex + 1) {
+                    locs.add(Pair(type, legs[iterator].locs[jiterator]))
+                    jiterator++
+                }
+                iterator++
+            }
+            return locs
+        }
 
+        private fun parseTime(time: String): String {
+            val parser = SimpleDateFormat("yyyyMMddHHmm", Locale("fi"))
+            val formatter = SimpleDateFormat("HH:mm", Locale("fi"))
+            val calendar = Calendar.getInstance()
+            calendar.time = parser.parse(time)
+            return formatter.format(calendar.time)
+        }
 
-    // Create new views (invoked by the layout manager)
+        inner class CustomGridLayoutManager(context: Context, orientation: Int) : LinearLayoutManager(context, orientation, false) {
+            private var isScrollEnabled = true
+
+            fun setScrollEnabled(flag: Boolean) {
+                this.isScrollEnabled = flag
+            }
+
+            override fun canScrollVertically(): Boolean {
+                return isScrollEnabled && super.canScrollVertically()
+            }
+
+            override fun canScrollHorizontally(): Boolean {
+                return isScrollEnabled && super.canScrollHorizontally()
+            }
+        }
+
+        fun bind(route: Route, context: Context, clickListener: (Route) -> Unit) {
+            cardView.findViewById<TextView>(R.id.routeDepartsTextView).text = parseTime(route.legs.first().locs.first().depTime)
+            cardView.findViewById<TextView>(R.id.routeArrivesTextView).text = parseTime(route.legs.last().locs.last().arrTime)
+
+            val legsViewManager = CustomGridLayoutManager(context, LinearLayoutManager.HORIZONTAL)
+            legsViewManager.setScrollEnabled(false)
+            val legsViewAdapter = RoutesLegCardAdapter(route.legs)
+            val legsRecyclerView = cardView.findViewById<RecyclerView>(R.id.routeLegsRecyclerView)
+            legsRecyclerView.apply {
+                // use this setting to improve performance if you know that changes
+                // in content do not change the layout size of the RecyclerView
+                setHasFixedSize(true)
+                layoutManager = legsViewManager
+                adapter = legsViewAdapter
+            }
+            val infoViewManager = LinearLayoutManager(context)
+
+            val selectedIndicator = cardView.findViewById<ImageView>(R.id.resultSelectedmageView)
+
+            val infoViewAdapter = RoutesInfoCardAdapter(combineLegs(route.legs))
+            val infoRecyclerView = cardView.findViewById<RecyclerView>(R.id.routeInfoRecyclerView)
+            infoRecyclerView.apply {
+                // use this setting to improve performance if you know that changes
+                // in content do not change the layout size of the RecyclerView
+                setHasFixedSize(true)
+                layoutManager = infoViewManager
+                adapter = infoViewAdapter
+            }
+            infoRecyclerView.visibility = View.GONE
+            // TODO Cannot set listener directly, pass into items inside it
+            infoRecyclerView.setOnClickListener {
+                clickListener(route)
+            }
+
+            var infoVisible: Boolean = false
+            // TODO Change arrow when view is open
+            val showMoreButton = cardView.findViewById<ImageButton>(R.id.showRouteButton)
+            showMoreButton.setOnClickListener {
+                if(!infoVisible) {
+                    infoRecyclerView.visibility = View.VISIBLE
+                    infoVisible = !infoVisible
+                    val caretUp = R.drawable.ic_arrowup_glyph
+                    showMoreButton.setImageResource(caretUp)
+                }
+                else {
+                    infoRecyclerView.visibility = View.GONE
+                    infoVisible = !infoVisible
+                    val caretDown = R.drawable.ic_arrowdown_glyph
+                    showMoreButton.setImageResource(caretDown)
+                }
+            }
+            cardView.setOnClickListener {
+                clickListener(route)
+            }
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): RoutesResultsCardAdapter.MyViewHolder {
-        // create a new view
+                                    viewType: Int): RoutesResultsCardAdapter.RouteResultViewHolder {
         val cardView = LayoutInflater.from(parent.context)
                 .inflate(R.layout.route_card, parent, false) as CardView
-        // set the view's size, margins, paddings and layout parameters
-
-        return MyViewHolder(cardView)
+        return RouteResultViewHolder(cardView)
     }
 
-    inner class CustomGridLayoutManager(context: Context, orientation: Int) : LinearLayoutManager(context, orientation, false) {
-        private var isScrollEnabled = true
-
-        fun setScrollEnabled(flag: Boolean) {
-            this.isScrollEnabled = flag
-        }
-
-        override fun canScrollVertically(): Boolean {
-            //Similarly you can customize "canScrollHorizontally()" for managing horizontal scroll
-            return isScrollEnabled && super.canScrollVertically()
-        }
-
-        override fun canScrollHorizontally(): Boolean {
-            //Similarly you can customize "canScrollHorizontally()" for managing horizontal scroll
-            return isScrollEnabled && super.canScrollHorizontally()
-        }
+    override fun onBindViewHolder(holder: RouteResultViewHolder, position: Int) {
+        holder.bind(inputData[position], mContext, clickListener)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        if(position.equals(0)) {
-            mResultSelected = true
-        }
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.cardView.findViewById<TextView>(R.id.routeDepartsTextView).text = parseTime(inputData[position].legs.first().locs.first().depTime)
-        holder.cardView.findViewById<TextView>(R.id.routeArrivesTextView).text = parseTime(inputData[position].legs.last().locs.last().arrTime)
-
-        val legsViewManager = CustomGridLayoutManager(this.mContext, LinearLayoutManager.HORIZONTAL)
-        legsViewManager.setScrollEnabled(false)
-        val legsViewAdapter = RoutesLegCardAdapter(inputData[position].legs)
-        val legsRecyclerView = holder.cardView.findViewById<RecyclerView>(R.id.routeLegsRecyclerView)
-        legsRecyclerView.apply {
-            // use this setting to improve performance if you know that changes
-            // in content do not change the layout size of the RecyclerView
-            setHasFixedSize(true)
-            layoutManager = legsViewManager
-            adapter = legsViewAdapter
-        }
-        val infoViewManager = LinearLayoutManager(this.mContext)
-
-        val selectedIndicator = holder.cardView.findViewById<ImageView>(R.id.resultSelectedmageView)
-        if(mResultSelected) {
-            selectedIndicator.setBackgroundColor(mContext.getColor(R.color.colorPrimary))
-        }
-        else {
-            selectedIndicator.visibility = View.GONE
-        }
-
-        val infoViewAdapter = RoutesInfoCardAdapter(combineLegs(inputData[position].legs))
-        val infoRecyclerView = holder.cardView.findViewById<RecyclerView>(R.id.routeInfoRecyclerView)
-        infoRecyclerView.apply {
-            // use this setting to improve performance if you know that changes
-            // in content do not change the layout size of the RecyclerView
-            setHasFixedSize(true)
-            layoutManager = infoViewManager
-            adapter = infoViewAdapter
-        }
-        infoRecyclerView.visibility = View.GONE
-        // TODO Change arrow when view is open
-        holder.cardView.findViewById<ImageButton>(R.id.showRouteButton).setOnClickListener {
-            if(!mInfoVisible) {
-                infoRecyclerView.visibility = View.VISIBLE
-                mInfoVisible = !mInfoVisible
-            }
-            else {
-                infoRecyclerView.visibility = View.GONE
-                mInfoVisible = !mInfoVisible
-            }
-        }
-    }
-
-    private fun combineLegs(legs: List<Leg>): ArrayList<Pair<String?, Location>> {
-        var iterator = 0
-        val locs = ArrayList<Pair<String?, Location>>()
-        while(iterator < legs.lastIndex + 1) {
-            val type = legs[iterator].code
-            var jiterator = 0
-            while(jiterator < legs[iterator].locs.lastIndex + 1) {
-                locs.add(Pair(type, legs[iterator].locs[jiterator]))
-                jiterator++
-            }
-            iterator++
-        }
-        return locs
-    }
-
-    private fun parseTime(time: String): String {
-        val parser = SimpleDateFormat("yyyyMMddHHmm", Locale("fi"))
-        val formatter = SimpleDateFormat("HH:mm", Locale("fi"))
-        val calendar = Calendar.getInstance()
-        calendar.time = parser.parse(time)
-        return formatter.format(calendar.time)
-    }
-
-    // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = inputData.size
 }
